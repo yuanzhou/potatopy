740b2d1619e48b11958b7150f3de786053516c78
Zhou Yuan <yuanzhou19@gmail.com>
2014-06-20 15:17:41 -0400

    Added HTTP response status

--- potatopy/core/manager.py
+++ potatopy/core/manager.py
@@ -66,5 +66,88 @@ class Manager(object):
 
         response_content = options['content']
 
-        # Return the response as a dictionary (contains 'headers' and 'content')
-        return {'headers': response_headers, 'content': response_content}
+        # options must be a dictionary
+        if options.has_key('status'):
+            # A dictionary that contains all the HTTP status codes and according reason phrases
+            status_map = {
+                # 1xx: Informational - Request received, continuing process
+                100: 'Continue',
+                101: 'Switching Protocols',
+                102: 'Processing',
+                # 2xx: Success - The action was successfully received, understood, and accepted
+                200: 'OK',
+                201: 'Created',
+                202: 'Accepted',
+                203: 'Non-Authoritative Information',
+                204: 'No Content',
+                205: 'Reset Content',
+                206: 'Partial Content',
+                207: 'Multi-Status',
+                208: 'Already Reported',
+                226: 'IM Used',
+                # 3xx: Redirection - Further action must be taken in order to complete the request
+                300: 'Multiple Choices',
+                301: 'Moved Permanently',
+                302: 'Found',
+                303: 'See Other',
+                304: 'Not Modified',
+                305: 'Use Proxy',
+                307: 'Temporary Redirect',
+                308: 'Permanent Redirect',
+                # 4xx: Client Error - The request contains bad syntax or cannot be fulfilled
+                400: 'Bad Request',
+                401: 'Unauthorized',
+                402: 'Payment Required',
+                403: 'Forbidden',
+                404: 'Not Found',
+                405: 'Method Not Allowed',
+                406: 'Not Acceptable',
+                407: 'Proxy Authentication Required',
+                408: 'Request Timeout',
+                409: 'Conflict',
+                410: 'Gone',
+                411: 'Length Required',
+                412: 'Precondition Failed',
+                413: 'Request Entity Too Large',
+                414: 'Request-URI Too Long',
+                415: 'Unsupported Media Type',
+                416: 'Requested Range Not Satisfiable',
+                417: 'Expectation Failed',
+                422: 'Unprocessable Entity',
+                423: 'Locked',
+                424: 'Failed Dependency',
+                425: 'Reserved for WebDAV advanced collections expired proposal',
+                426: 'Upgrade required',
+                428: 'Precondition Required',
+                429: 'Too Many Requests',
+                431: 'Request Header Fields Too Large',
+                # 5xx: Server Error - The server failed to fulfill an apparently valid request
+                500: 'Internal Server Error',
+                501: 'Not Implemented',
+                502: 'Bad Gateway',
+                503: 'Service Unavailable',
+                504: 'Gateway Timeout',
+                505: 'HTTP Version Not Supported',
+                506: 'Variant Also Negotiates (Experimental)',
+                507: 'Insufficient Storage',
+                508: 'Loop Detected',
+                510: 'Not Extended',
+                511: 'Network Authentication Required'
+            }
+     
+            if status_map.has_key(options['status']):
+                # String consisting of Status-Code(cast int into string) and Reason-Phrase, separated by a single space
+                response_status = str(options['status']) + ' ' + status_map[options['status']]
+                
+                # Must not contain a message body for 204 and 304
+                if (options['status'] == 204 or options['status'] == 304):
+                    response_content = ''
+
+            else:
+                Logger.log(APP_LOG_DIR, 'ERROR', __name__, 'The provided HTTP status code is invalid!')
+
+        else:
+            response_status = '200 OK'
+
+        # Return the response as a dictionary (contains 'status', headers' and 'content')
+        return {'status': response_status, 'headers': response_headers, 'content': response_content}
